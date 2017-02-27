005a92bf7260e221a84d7dc9fa11cfbd509834a2
Dazzy Ding <h@dazzyd.org>
2017-02-27 21:33:17 +0800

    bump

--- poi.js
+++ poi.js
@@ -2,21 +2,13 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-function safeParseInt(string) {
-  const number = Number(string)
-  if (Number.isNaN(number)) {
-    throw new Error(`Parse int failed: ${string}`)
-  }
-  return number
-}
-
-(() => {
+;(() => {
   const final = {}
   for(const fpath of fs.walkSync('./poi')) {
     const m = path.basename(fpath).match(/^(\d+)_(\d+)\.json$/)
     if (m == null) continue
-    area = safeParseInt(m[1])
-    cell = safeParseInt(m[2])
+    const area = Number(m[1])
+    const cell = Number(m[2])
     final[`${area}-${cell}`] = fs.readJSONSync(fpath)
   }
   fs.writeJSONSync('./poi/final.json', final, {spaces: ''})
